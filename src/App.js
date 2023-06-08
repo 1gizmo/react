ab392cdc1388e34b1cf96436fe4bca0cd82e8d3e
1gizmo <126743255+1gizmo@users.noreply.github.com>
2023-06-08 17:55:42 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoTemplate from './component/TodoTemplate';
+import TodoTemplate from './component/todo/TodoTemplate';
+import Hader from './component/layout/Hader';
+import Footer from './component/layout/Footer';
+import { Login } from '@mui/icons-material';
+import { Route, Routes } from 'react-router-dom';
+import Join from './component/user/Join';
 
 function App() {
   return (
-    <TodoTemplate />
+    <>
+    <Hader />
+    
+    <Routes>
+    <Route path='/' element={<TodoTemplate />} />
+    <Route path='/login'element={<Login/>} />
+    <Route path='join' element={<Join />} />
+    </Routes>
+
+    <Footer />
+    </>
   );
 }
 
